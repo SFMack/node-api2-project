f4c0f3fd9e4e3f8f387a4326add7c29c05c7fe2f
SFMack <sfcamack@icloud.com>
2020-05-10 23:14:02 -0700

    get a specific post and add a post functionality working

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ const server = express();
 server.use(express());
 server.use(express.json());
 
-server.get("/", (req, res) => {
+// get all posts
+server.get("/api/posts", (req, res) => {
   db.find()
     .then(posts => {
       res.status(200).json(posts);
@@ -15,9 +16,37 @@ server.get("/", (req, res) => {
     .catch(error => {
       console.log(error);
       res.status(500).json({
-        message: "Error retrieving the data",
+        error: "The posts information could not be retrieved.",
       });
     });
+});
+
+// get a specific post
+server.get("/api/posts/:id", (req, res) => {
+  const { id } = req.params;
+
+  db.findById(id).then(post => {
+    if (post) {
+      res.status(200).json(post);
+    } else {
+      res
+        .status(404)
+        .json({ message: "The post with the specified ID does not exist." });
+    }
+  });
+});
+
+// add a post
+server.post("/api/posts", (req, res) => {
+  const newPost = req.body;
+  console.log(newPost);
+  db.insert(newPost)
+    .then(post => {
+      res.status(201).json(post);
+    })
+    .catch(err =>
+      res.status(400).json({ message: `User was not created\n${err}` })
+    );
 });
 
 module.exports = server;
